0c439df741d59535cce8513d710253562533712a
Nicolás Vidal Quini <80220574+nicolasvidal93@users.noreply.github.com>
2024-06-04 16:31:49 +0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -9,7 +9,7 @@ const CONFIG = {
    * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
    * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
    */
-  base: '/nevqportfolio/',
+  base: '/https://nicolasvidal93.github.io/portfolio//',
   projects: {
     github: {
       display: true, // Display GitHub projects?
