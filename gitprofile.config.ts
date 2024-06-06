dbd61aace26173454684c7459afe6915e8cccc42
Nicolás Vidal Quini <80220574+nicolasvidal93@users.noreply.github.com>
2024-06-06 11:54:52 +0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -143,13 +143,13 @@ const CONFIG = {
         'Remote sensing is essential for analyzing spatial-temporal phenomena in Environmental Geography. This study uses Synthetic Aperture RADAR (SAR) images to characterize a shallow lake and its wetlands via backscattering parameters. Principal component analysis on Sentinel 1 C images identifies potential wetlands for other land uses, aiding in understanding environmental functionality for risk and vulnerability assessments.',
     },
     {
-      title: 'Environmental characterization of pampean wetlands through multi-temporal analysis sentinel 1',
-      conferenceName: 'CIOTTIG 2022',
-      journalName: 'Geografía y Sistemas de Información Geográfica ISSN 1852-8031',
-      authors: 'Vidal Quini, N. E., Lucía M. Laffeuillade, and Geraldi, A. M. ',
-      link: 'https://revistageosig.wixsite.com/geosig/geosig-27-2023',
+      title: ' Methodology for the analysis of the frequency of shallow water bodies in the Pampean region, Argentina',
+      conferenceName: '',
+      journalName: ' Investigaciones Geográficas',
+      authors: 'Vidal Quini, N. E., and Geraldi,A. M. ',
+      link: 'https://doi.org/10.14198/INGEO.23410',
       description:
-        'This 35-year study of shallow lakes in Argentina's Pampa region utilizes satellite imagery and meteorological data. It reveals that temperature has a more significant impact than precipitation on the presence or desiccation of these water bodies, emphasizing the importance of understanding and addressing environmental risks in the region',
+        '',
     },
      {
       title: 'Environmental characterization of pampean wetlands through multi-temporal analysis sentinel 1',
