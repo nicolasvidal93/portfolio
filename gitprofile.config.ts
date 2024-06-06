25ad3477cc9f8180fdc20486a55627020b2427b5
Nicolás Vidal Quini <80220574+nicolasvidal93@users.noreply.github.com>
2024-06-06 15:16:14 +0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -36,7 +36,7 @@ const CONFIG = {
           title: '“ASPO effect (Preventive and Mandatory Social Isolation) in the atmosphere of the southwest of the province of Buenos Aires, Argentine Republic',
           description:
             'The web visualizer is used to display the results of nitrogen dioxide (NO2) monitoring in the southwest of Buenos Aires province during the period from March to July 2020 using images from the Sentinel 5-P satellite.',
-          imageUrl:'https://www.labgeot.uns.edu.ar/no2/#6/-40.513/-59.227',
+          imageUrl:'https://github.com/nicolasvidal93/portfolio/blob/main/portal_no2.PNG',
           link: 'https://www.labgeot.uns.edu.ar/no2/',
         },
         {
