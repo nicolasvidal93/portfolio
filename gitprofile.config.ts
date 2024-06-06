6022e1212912ec7888256f440387d69de4fddd77
Nicolás Vidal Quini <80220574+nicolasvidal93@users.noreply.github.com>
2024-06-06 15:57:56 +0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -78,7 +78,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'CV_Nicolas_Vidal_Quini_git.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://github.com/nicolasvidal93/README/blob/main/CV_Nicolas_Vidal_Quini_git.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
    'Google Earth Engine',
