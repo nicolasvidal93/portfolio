9de4e40dfcd791ec5f80066ec4a299dcf0a6f7ad
Nicolás Vidal Quini <80220574+nicolasvidal93@users.noreply.github.com>
2024-06-06 15:39:20 +0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -43,8 +43,7 @@ const CONFIG = {
           title: 'Southwest Buenos Aires Geographic Information Catalog',
           description:
             'Normalization of vector and raster data for the Southwest of Buenos Aires province according to the Argentinian Infrastructure Spatial Data. With the aim of surveying the distribution of shallow water bodies and potential wetlands in the Encadenadas del Oeste basin, a methodology was developed to show the frequency distribution of the presence and/or absence of water during dry and wet periods from 1986 to 2021 using NDWI from Landsat images.',
-          imageUrl:
-            'https://raw.githubusercontent.com/nicolasvidal93/portfolio/main/portal_IDESoB.PNG',
+          imageUrl:'https://raw.githubusercontent.com/nicolasvidal93/portfolio/main/portal_IDESoB.PNG',
           link: 'https://catalogo-ig.labgeot.uns.edu.ar/geonetwork/srv/spa/catalog.search#/home',
         },
       ],
